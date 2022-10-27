d1f777ea70a3f50f1aa59a9fa93c92cb2f72dc45
Rubén Carrión <cnrubn@gmail.com>
2022-10-27 15:58:16 +0200

    Nuevo método para introducir los tags.

--- src/app/tareas/home/home.component.ts
+++ src/app/tareas/home/home.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { CrudService } from 'src/app/services/crud.service';
@@ -16,10 +16,16 @@ export class HomeComponent implements OnInit {
   miTarea: FormGroup = this.fb.group({
 
     tit: [ '', [ Validators.required, Validators.minLength(3), Validators.maxLength(20) ] ],
-    txt: [ '', [ Validators.required, Validators.minLength(3) ] ], 
-    tags: [ '', [ Validators.required, Validators.minLength(3) ] ], 
+    txt: [ , [ Validators.required, Validators.minLength(3) ] ], 
+    tags: this.fb.array( [], Validators.required ), 
 
   });
+
+  nuevoTag: FormControl = this.fb.control( '', [ Validators.required, Validators.minLength(3), Validators.maxLength(15) ] );
+
+  get tagsArr() {
+    return this.miTarea.get( 'tags' ) as FormArray;
+  }
 
   listadoTareas: Tarea[] = [];
   listadoRespuesta: infoUsuario = {};
@@ -31,6 +37,10 @@ export class HomeComponent implements OnInit {
 
   modificarActivacion: number = -1;
   cargando: boolean = true;
+
+  modActivo: boolean = false;
+
+  // avisosInc: boolean = false;
 
   constructor( private router: Router,
                private auth: AuthService,
@@ -49,7 +59,7 @@ export class HomeComponent implements OnInit {
 
       })
       
-    }, 1000 );
+    }, 500 );
 
   }
 
@@ -61,6 +71,8 @@ export class HomeComponent implements OnInit {
   }
 
   guardarTarea() {
+
+    console.log(this.miTarea)
 
     const tarea = this.miTarea.value; 
       
@@ -99,22 +111,132 @@ export class HomeComponent implements OnInit {
     this.tareaModificar = '';
     this.miTarea.reset();
 
+    this.tagsArr.reset();
+
+
+    // Resetear tags
+    if ( this.modActivo ) {
+
+      for ( let i of this.tagsArr.value ){
+
+        this.tagsArr.removeAt( i );
+
+      }
+
+    }
+
+    this.modActivo = false;
+
+
   }
 
   modificar( index: number ) {
 
+    // En caso de pulsar 2 o más veces seguidas el botón modificar es necesario hacer un reset de tags.
+    this.modActivo = true;
+
+    if ( this.modActivo ) {
+
+      for ( let i of this.tagsArr.value ){
+
+        this.tagsArr.removeAt( i );
+
+      }
+
+ 
+    }
+
+    console.log(this.tagsArr.value)
+
     this.modificarActivacion = index;
 
     this.tareaModificar = this.listadoRespuesta.tareas![ index ];
+
+    // Preparando array tags.
+    for( let tag of this.tareaModificar.tags ) {
+
+
+      this.tagsArr.push( this.fb.control( tag, Validators.required ) );
+
+
+
+    }
+
+    // this.tagsArr.push( this.fb.control( this.tareaModificar.tags, Validators.required ) );
+    
+    // console.log(this.tagsArr)
+    // console.log(this.tareaModificar)
+  }
+
+  agregarTag() {
+
+    // console.log(this.tagsArr)
+
+    if ( this.nuevoTag.invalid ) { return; }
+
+    // this.tagsArr.push( new FormControl( this.nuevoTag.value, Validators.required ) );
+    this.tagsArr.push( this.fb.control( this.nuevoTag.value, Validators.required ) );
+
+    this.nuevoTag.reset();
+
+  }
+
+  eliminarTag( index: number ) {
+
+    this.tagsArr.removeAt( index );
     
   }
 
   // COMPROBACIONES VISOR TAREAS
   campoTamValido( data: string ) {
-
+   
     return this.miTarea.controls[data].errors &&
     this.miTarea.controls[data].touched;
     
   }
+
+  campoTagValido( data: string ) {
+
+    console.log(this.miTarea.errors)
+    console.log(this.miTarea)
+   
+    return this.miTarea.errors &&
+    this.miTarea.touched;
+    
+  }
+
+  // campoTagInfo() {
+
+    // console.log(this.miTarea)
+
+    // console.log(this.tagsArr)
+
+  //   return !this.miTarea.controls['tags'].touched;
+    
+  // }
+
+  // comprobarVisor() {
+  //   console.log(this.miTarea);
+  // }
+
+  // avisosIncompleto() {
+
+    // this.campoTamValido( 'tit' );
+    // this.campoTamValido( 'txt' );
+
+    // if ( this.miTarea.invalid ) {
+
+    //   this.avisosInc = true;
+    // }
+
+
+
+    // console.log(this.avisosInc)
+    // console.log(this.miTarea.invalid)
+
+    // [disabled]="miTarea.invalid"
+
+    
+  // }
 
 }
